5689e7a12cdbefb312ae7ae8e561d9314ca0374e
Bruno Kempfer Stein <brunokempferstein@gmail.com>
2024-01-22 14:34:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mia, eu te amo! Se você me ama, da uma risadinha!</h1>
+  return <h1>Mia, eu te amo! Se você me ama, da uma risadinha!</h1>;
 }
 
 export default Home;
